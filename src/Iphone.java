86cef15263c82a20d06a47009af57a4317249857
mariocb-duarte <mcbd.eng@gmail.com>
2024-03-03 09:43:49 -0300

    update

--- src/Iphone.java
+++ src/Iphone.java
@@ -1,5 +1,50 @@
-public class Iphone {
-    public static void main(String[] args) {
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet{
+
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música");
 
     }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando música: " + musica);
+    }
+
+    @Override
+    public void ligar() {
+        System.out.println("Ligando Iphone");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada telefônica");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correrio voz");
+    }
+
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionado aba");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página");
+    }
+
 }
